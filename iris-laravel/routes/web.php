0d1b374bf3be4ba0fc960e66421bdf4e40a6f59d
OGDogg-Dev <alkautsar.putra1922@gmail.com>
2025-07-31 21:09:44 +0700

    update project

--- iris-laravel/routes/web.php
+++ iris-laravel/routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IrisController;
 
-Route::get('/klasifikas', [IrisController::class, 'index']);
+Route::get('/klasifikasi', [IrisController::class, 'index']);
 Route::post('/klasifikasi', [IrisController::class, 'classify']);
